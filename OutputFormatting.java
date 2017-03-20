f01435ea9744b2e74c9566f9967e3cb844097048
Sarika Venkatesan <sarika.pamarthi@gmail.com>
2017-03-20 16:35:21 -0400

    formatting the output

---
+++ OutputFormatting.java
@@ -0,0 +1,21 @@
+package introduction;
+
+import java.util.Scanner;
+
+public class OutputFormatting {
+
+    public static void main(String[] args) {
+    	
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //sc.close();
+                //formatting the output
+                System.out.printf("%-14s %03d %n", s1, x);
+            }
+            System.out.println("================================");
+
+    }
+}
