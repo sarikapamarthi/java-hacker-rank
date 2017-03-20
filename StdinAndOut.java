1148311fa693b78b8c8fc6aae41d718174c7fd0a
Sarika Venkatesan <sarika.pamarthi@gmail.com>
2017-03-20 15:24:03 -0400

    accepting input from user and printing it on console

---
+++ StdinAndOut.java
@@ -0,0 +1,21 @@
+package Introductions;
+
+import java.util.*;
+
+public class StdinAndOut {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        //getting input from the user
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        // printing the output on the console
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
+
