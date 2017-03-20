7a70f9354953a7473c2c5f4359be7f8bf95942d8
Sarika Venkatesan <sarika.pamarthi@gmail.com>
2017-03-19 22:41:37 -0400

    first java program with print statement

---
+++ WelcomeToJava.java
@@ -0,0 +1,9 @@
+package Introductions;
+
+public class WelcomeToJava {
+
+    public static void main(String[] args) {
+        // Printing "Hello, World." on output console
+    	System.out.println("Hello, World.");
+    }
+}
