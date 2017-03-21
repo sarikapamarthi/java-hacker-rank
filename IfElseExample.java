efc545676b5a0601819c4bb6ebe0b6efe550434d
Sarika Venkatesan <sarika.pamarthi@gmail.com>
2017-03-20 22:00:02 -0400

    IF Else example in java

--- IfElseExample.java
+++ IfElseExample.java
@@ -1,98 +1,49 @@
-package Introductions;
-
-
-import java.util.*;
-
-
-
-public class IfElseExample {
-
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		String ans = "";
-		ans = checkingIfElse( n,  ans );
-		sc.close();
-
-	}
-
-
-public static String checkingIfElse(int n, String ans ){
-
-	// checking if n is odd
-			if (n % 2 == 1) {
-				ans = "Weird";
-			}
-			// if n is even
-			else {
-				// checking if n is inclusive of range 2 to 5
-				if (n >= 2 && n <= 5) {
-					System.out.println("Not Weird");
-				}
-
-				// checking if n is inclusive of range 6 to 20
-				else if (n >= 6 && n <= 20) {
-					System.out.println("Weird");
-				}
-				// if n is greater then 20
-				else {
-					System.out.println("Not Weird");
-				}
-
-			}
-			// printing the value for n
-			System.out.println(ans);
-			return ans;
-}
-}
-/*
- * If n is odd, print Weird If n is even and in the inclusive range of 2 to 5 ,
- * print Not Weird If n is even and in the inclusive range of 6 to 20, print
- * Weird If n is even and greater than 20, print Not Weird
- */rl = ssh://git@github.com/sarikapamarthi/java-hacker-rank.git
+package introduction;
 
 import java.util.*;
 
-
-
-public class IfElseExample {
-
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		String ans = "";
-
-		// checking if n is odd
-		if (n % 2 == 1) {
-			ans = "Weird";
-		}
-		// if n is even
-		else {
-			// checking if n is inclusive of range 2 to 5
-			if (n >= 2 && n <= 5) {
-				System.out.println("Not Weird");
-			}
-
-			// checking if n is inclusive of range 6 to 20
-			else if (n >= 6 && n <= 20) {
-				System.out.println("Weird");
-			}
-			// if n is greater then 20
-			else {
-				System.out.println("Not Weird");
-			}
-
-		}
-		// printing the value for n
-		System.out.println(ans);
-		sc.close();
-
-	}
-}
 /*
  * If n is odd, print Weird If n is even and in the inclusive range of 2 to 5 ,
  * print Not Weird If n is even and in the inclusive range of 6 to 20, print
  * Weird If n is even and greater than 20, print Not Weird
  */
+
+public class IfElseExample {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		String ans = checkingIfElse(n);
+		
+		System.out.println(ans);
+		sc.close();
+
+	}
+
+	public static String checkingIfElse(int n) {
+
+		String ans = "";
+		
+		// checking if n is odd
+		if (n % 2 == 1) {
+			ans = "Weird";
+		} else { // if n is even
+			// checking if n is inclusive of range 2 to 5
+			if (n >= 2 && n <= 5) {
+				// System.out.println("Not Weird");
+				ans = "Not Weird";
+			} else if (n >= 6 && n <= 20) { // checking if n is inclusive of range 6 to 20
+				// System.out.println("Weird");
+				ans = "Weird";
+			} else { // if n is greater then 20
+				// System.out.println("Not Weird");
+				ans = "Not Weird";
+			}
+		}
+		
+		// printing the value for n
+		return ans;
+	}
+}
