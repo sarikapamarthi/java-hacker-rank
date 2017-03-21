3427421059fafe8013e0dd5d692c3537584360fd
Sarika Venkatesan <sarika.pamarthi@gmail.com>
2017-03-20 22:08:58 -0400

    Junit test case

--- IfElseExampleJunit.java
+++ IfElseExampleJunit.java
@@ -13,5 +13,21 @@ public class IfElseExampleJunit {
 
 		assertEquals("Weird", result);
 	}
+	
+	@Test
+	public void testCase2() throws Exception {
+
+		String result = IfElseExample.checkingIfElse(3);
+
+		assertEquals("Weird", result);
+	}
+	
+	@Test
+	public void testCase3() throws Exception {
+
+		String result = IfElseExample.checkingIfElse(54);
+
+		assertEquals("Not Weird", result);
+	}
 }
 
