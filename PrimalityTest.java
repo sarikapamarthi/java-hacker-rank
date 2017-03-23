698fd9fffb800d8bc682c0511ed46a35ba9b61f6
Sarika Venkatesan <sarika.pamarthi@gmail.com>
2017-03-22 21:49:03 -0400

    Checking if the no is prime or not

---
+++ PrimalityTest.java
@@ -0,0 +1,20 @@
+package introduction;
+
+import java.util.*;
+import java.math.*;
+
+public class PrimalityTest {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		BigInteger n = in.nextBigInteger();
+		in.close();
+		if (n.isProbablePrime(100)) {
+			System.out.println("prime");
+		} else {
+			System.out.println("not prime");
+		}
+	}
+}
+
+
