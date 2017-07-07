d785f20f4ffc08de4c6f214b8fc0cb52f4eb6cdd
“sarika <sarika.pamarthi@gmail.com>
2017-07-06 17:02:10 -0700

    Welcome file

---
+++ MyWelcome.java
@@ -0,0 +1,11 @@
+package javaprograms;
+
+public class Welcome {
+
+    public static void main(String[] args) {
+    	System.out.println("Hello, World.");
+    	System.out.println("Hello, Java.");
+    }
+}
+
+
